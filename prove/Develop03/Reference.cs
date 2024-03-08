481a35765dc5de240583b1cb5049ef690a19eb6a
bradenholg8 <bradenholg8@gmail.com>
2024-03-08 10:44:11 -0700

    Second Commit for Prove 3

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,20 +1,37 @@
 public class Reference
 {
+    private string book;
+    private int startChapter;
     private int startVerse;
+    private int endChapter;
     private int endVerse;
 
     public Reference(string reference)
     {
-        string[] parts = reference.Split(':');
-        startVerse = int.Parse(parts[1].Split('-')[0]);
-        endVerse = parts[1].Contains('-') ? int.Parse(parts[1].Split('-')[1]) : startVerse;
+        string[] parts = reference.Split(' ');
+        book = parts[0];
+        string[] verses = parts[1].Split(':');
+        startChapter = int.Parse(verses[0]);
+        if (verses[1].Contains('-'))
+        {
+            string[] verseRange = verses[1].Split('-');
+            startVerse = int.Parse(verseRange[0]);
+            endVerse = int.Parse(verseRange[1]);
+        }
+        else
+        {
+            startVerse = int.Parse(verses[1]);
+            endVerse = startVerse;
+        }
     }
 
     public string GetFormattedReference()
     {
-        if (startVerse == endVerse)
-            return $"{startVerse}";
+        if (startChapter == endChapter && startVerse == endVerse)
+            return $"{book} {startChapter}:{startVerse}";
+        else if (startChapter == endChapter)
+            return $"{book} {startChapter}:{startVerse}-{endVerse}";
         else
-            return $"{startVerse}-{endVerse}";
+            return $"{book} {startChapter}:{startVerse} - {endChapter}:{endVerse}";
     }
 }
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,14 +1,29 @@
 public class Word
 {
     private string text;
+    private bool hidden;
 
     public Word(string text)
     {
         this.text = text;
+        hidden = false;
     }
 
     public string GetText()
     {
-        return text;
+        if (hidden)
+            return "_";
+        else
+            return text;
+    }
+
+    public void Hide()
+    {
+        hidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return hidden;
     }
 }
